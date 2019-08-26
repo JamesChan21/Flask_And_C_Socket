25e5231094fd82bce114657a98dab88f42de2d67
JamesChan21 <724219735@qq.com>
2019-08-26 21:01:59 +0800

    Update socket_main.c

--- socket_main.c
+++ socket_main.c
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
 	int sockfd, ret, port;
 	char address[16], buf[4096], type[7], para[128], message[256];
 	struct sockaddr_in servaddr;
-    fd_set   t_set1;
-    struct timeval  tv;
+	fd_set   t_set1;
+	struct timeval  tv;
 
 	if(argc <= 3){
 		printf("api <address> <port> <GET/POST/PUT/DELETE> <PARA>\r\n");
@@ -37,26 +37,26 @@ int main(int argc, char *argv[])
 	
 
 	/*create socket*/
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
-	        printf("create socket error!\n");
-	        exit(0);
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) {
+		printf("create socket error!\n");
+		exit(0);
 	};
 	printf("successfully create socket!\r\n");
 
-    bzero(&servaddr, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port);
+	bzero(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(port);
 
-    if (inet_pton(AF_INET, address, &servaddr.sin_addr) <= 0 ){
-            printf("inet_pton error!\n");
-            exit(0);
-    };
+	if (inet_pton(AF_INET, address, &servaddr.sin_addr) <= 0 ){
+	    printf("inet_pton error!\n");
+	    exit(0);
+	};
 
-    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
-            printf("connect error!\n");
-            exit(0);
-    }
-    printf("successfully connect socket!\r\n");
+	if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0){
+	    printf("connect error!\n");
+	    exit(0);
+	}
+	printf("successfully connect socket!\r\n");
 
 	
 	/*GET*/
@@ -78,10 +78,10 @@ int main(int argc, char *argv[])
 	}
 
 	/*WRITE*/
-    if (ret = write(sockfd, message, sizeof(message)) < 0) {
-        printf("Send failed! ret:%d\r\n", ret);
-        return 1;
-    }
+	if (ret = write(sockfd, message, sizeof(message)) < 0) {
+	printf("Send failed! ret:%d\r\n", ret);
+	return 1;
+	}
 
 	/*READ*/
 	ret = read(sockfd, buf, 4096 - 1);
